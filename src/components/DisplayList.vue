e6203885493973b007fbdc91711aaf60ba0558b9
IsTree <IsTree@gmail.com>
2023-06-19 09:07:05 +0800

    完成

--- src/components/DisplayList.vue
+++ src/components/DisplayList.vue
@@ -2,22 +2,24 @@
 defineProps({
   todoList: Array,
   default() {
-      return [];
-    },
+    return [];
+  },
 });
-const emit = defineEmits(["toogle-completed"])
-const handleCompleted = (id)=>{
-    emit("toogle-completed",id)
-}
-const handleDelete = (id) =>{
-  emit("delete-todo",id)
-}
+const emit = defineEmits(["toogle-completed", "delete-todo"]);
+const handleCompleted = (id) => {
+  emit("toogle-completed", id);
+};
+const handleDelete = (id) => {
+  emit("delete-todo", id);
+};
 </script>
 <template>
   <ul>
     <li v-for="todo in todoList" v-bind:key="todo.id">
       {{ todo.content }}
-      <button @click="handleCompleted(todo.id)" v-if="!todo.completed">未完成</button>
+      <button @click="handleCompleted(todo.id)" v-if="!todo.completed">
+        未完成
+      </button>
       <button @click="handleCompleted(todo.id)" v-else>完成</button>
       <button @click="handleDelete(todo.id)">刪除</button>
     </li>
--- src/components/TodoPage.vue
+++ src/components/TodoPage.vue
@@ -7,25 +7,28 @@ const handleSubmit = (todo) => {
   todoList.push(todo);
   console.log(todo, todoList);
 };
-const toggleCompleted=(id)=>{
-    todoList.forEach((todo)=>{
-      if(todo.id === id){
-        todo.completed= !todo.completed
-        // console.log(todoList)
-      }
-    })
-}
-const deleteTodo=(id)=>{
-    todoList.forEach((todo)=>{
-      if(todo.id === id){
-        todo.completed= !todo.completed
-        // console.log(todoList)
-      }
-    })
-}
+const toggleCompleted = (id) => {
+  todoList.forEach((todo) => {
+    if (todo.id === id) {
+      todo.completed = !todo.completed;
+      // console.log(todoList)
+    }
+  });
+};
+const deleteTodo = (id) => {
+  todoList.forEach((todo, index) => {
+    if (todo.id === id) {
+      todoList.splice(index, 1);
+    }
+  });
+};
 </script>
 
 <template>
   <InputTodo @todo-submitted="handleSubmit" />
-  <DisplayList :todoList="todoList" @toogle-completed="toggleCompleted" @delete-todo=""/>
+  <DisplayList
+    :todoList="todoList"
+    @toogle-completed="toggleCompleted"
+    @delete-todo="deleteTodo"
+  />
 </template>
